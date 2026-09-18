446b8e3c52e111ef816374d87ea8466e39fef5a6
Satyam Patel <satyamp9797@gmail.com>
2026-09-18 23:47:17 +0530

    removed unused imports

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 //import AppWithContextAPI from "./AppWithContextAPI";
-import "./styles.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
