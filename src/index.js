202344c6ef17c6220969eb7f4e81882d7cea4687
Satyam Patel <56593183+satyam-7777@users.noreply.github.com>
2026-09-18 23:58:41 +0530

    Import index.css for styling

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
+import "./index.css";
 //import AppWithContextAPI from "./AppWithContextAPI";
+
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
